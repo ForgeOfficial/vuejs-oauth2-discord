a9ae806c4591ddd2a1d8576e88ec0cc3797789ed
ForgeOff <86209935+ForgeOfficial@users.noreply.github.com>
2021-07-21 09:16:17 +0200

    Init Class VueOauth2Discord

--- vuejs/index.js
+++ vuejs/index.js
@@ -1,42 +1,48 @@
 import axios from 'axios';
 
-let VueOauth2DiscordConfig = {
-    host: 'localhost',
-    port: 5000,
-    ssl: false,
-    withCredentials: true,
-    path: `/discord`,
-    headers: {}
+class VueOauth2Discord {
+    constructor() {
+        this.vueOauth2DiscordConfig = {
+            host: 'localhost',
+            port: 5000,
+            ssl: false,
+            withCredentials: true,
+            path: `/discord`,
+            headers: {}
+        }
+
+        this.uri = this.generateURI();
+    }
+
+    init(app, vueOauth2DiscordConfig) {
+        this.vueOauth2DiscordConfig = vueOauth2DiscordConfig;
+        this.uri = this.generateURI();
+        app.config.globalProperties.oauth2discord.login = this.login;
+    }
+
+    async login(component, redirectTo) {
+        return new Promise((resolve, reject) => {
+            axios.post(this.uri, {
+                redirectTo: redirectTo ?? null
+            }, {
+                withCredentials: this.vueOauth2DiscordConfig.withCredentials,
+                headers: this.vueOauth2DiscordConfig.headers
+            }).then(response => {
+                component.isAuthenticated = response.data['isAuthenticated'];
+
+                if (response.data['isAuthenticated'])
+                    component.user = response.data['user'];
+
+                if (response.data['redirect'])
+                    window.location.href = response.data['redirect'];
+            }).catch(e => reject(e));
+
+        })
+    }
+
+    generateURI() {
+        return `http${this.vueOauth2DiscordConfig.ssl ? 's' : ''}://${this.vueOauth2DiscordConfig.host}${[80, 8080].includes(this.vueOauth2DiscordConfig.port) ? '' : `:${this.vueOauth2DiscordConfig.port}`}${this.vueOauth2DiscordConfig.path}`;
+    }
 }
 
-let uri = generateURI();
-
-export async function login(component, redirectTo) {
-    return new Promise((resolve, reject) => {
-        axios.post(uri, {
-            redirectTo: redirectTo ?? null
-        }, {
-            withCredentials: VueOauth2DiscordConfig.withCredentials,
-            headers: VueOauth2DiscordConfig.headers
-        }).then(response => {
-            component.isAuthenticated = response.data['isAuthenticated'];
-
-            if (response.data['isAuthenticated'])
-                component.user = response.data['user'];
-
-            if (response.data['redirect'])
-                window.location.href = response.data['redirect'];
-        }).catch(e => reject(e));
-
-    })
-}
-
-export default function (app, vueOauth2DiscordConfig) {
-    VueOauth2DiscordConfig = vueOauth2DiscordConfig;
-    uri = generateURI();
-    app.config.globalProperties.oauth2discord.login = login;
-}
-
-function generateURI() {
-    return `http${VueOauth2DiscordConfig.ssl ? 's' : ''}://${VueOauth2DiscordConfig.host}${[80, 8080].includes(VueOauth2DiscordConfig.port) ? '' : `:${VueOauth2DiscordConfig.port}`}${VueOauth2DiscordConfig.path}`;
-}
+module.exports = VueOauth2Discord;
